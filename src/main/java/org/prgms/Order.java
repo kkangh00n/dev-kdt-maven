e4b773c51c1151d6467b12c2186b8c6b221ead80
kkangh00n <kig2454@gmail.com>
2023-10-11 21:51:50 +0900

    refactor: Order -> Voucher 인터페이스 의존

--- src/main/java/org/prgms/Order.java
+++ src/main/java/org/prgms/Order.java
@@ -8,21 +8,21 @@ public class Order {
     private final UUID orderId;
     private final UUID customerId;
     private final List<OrderItem> orderItems;
-    private FixedAmountVoucher fixedAmountVoucher;
+    private Voucher voucher;
     private OrderStatus orderStatus = OrderStatus.ACCEPTED;
 
-    public Order(UUID orderId, UUID customerId, List<OrderItem> orderItems, long discoundAmount) {
+    public Order(UUID orderId, UUID customerId, List<OrderItem> orderItems, Voucher voucher) {
         this.orderId = orderId;
         this.customerId = customerId;
         this.orderItems = orderItems;
-        this.fixedAmountVoucher = new FixedAmountVoucher(discoundAmount);
+        this.voucher = voucher;
     }
 
     public long totalAmount(){
         Long beforeDiscount = orderItems.stream().map(v -> v.getProductPrice() * v.getQuantity())
             .reduce(0L, Long::sum);
 
-        return fixedAmountVoucher.discount(beforeDiscount);
+        return voucher.discount(beforeDiscount);
     }
 
     public void setOrderStatus(OrderStatus orderStatus) {
--- src/main/java/org/prgms/PercentDiscountVoucher.java
+++ src/main/java/org/prgms/PercentDiscountVoucher.java
@@ -2,22 +2,23 @@ package org.prgms;
 
 import java.util.UUID;
 
-public class FixedAmountVoucher implements Voucher{
+public class PercentDiscountVoucher implements Voucher{
 
     private final UUID voucherId;
     private final long amount;
 
-    public FixedAmountVoucher(UUID voucherId, long amount) {
+    public PercentDiscountVoucher(UUID voucherId, long amount) {
         this.voucherId = voucherId;
         this.amount = amount;
     }
 
     @Override
     public UUID getVoucherId() {
-        return voucherId;
+        return null;
     }
 
-    public long discount(long beforeDiscount){
-        return beforeDiscount - amount;
+    @Override
+    public long discount(long beforeDiscount) {
+        return 0;
     }
 }
