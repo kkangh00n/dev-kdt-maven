03f3f6a91f015670f653f6f81bff79bb8fe89faf
kkangh00n <kig2454@gmail.com>
2023-10-12 16:31:21 +0900

    refactor: 실행 클래스 변경

--- src/main/java/org/prgms/Main.java
+++ src/main/java/org/prgms/Main.java
@@ -1,8 +1,35 @@
 package org.prgms;
 
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.UUID;
+import org.prgms.order.Order;
+import org.prgms.order.OrderItem;
+import org.prgms.order.OrderService;
+import org.prgms.voucher.FixedAmountVoucher;
+import org.prgms.voucher.Voucher;
+import org.prgms.voucher.VoucherRepository;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.util.Assert;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+            AppConfiguration.class);
+
+        UUID customerId = UUID.randomUUID();
+
+        VoucherRepository voucherRepository = applicationContext.getBean(VoucherRepository.class);
+        Voucher voucher = voucherRepository.insert(new FixedAmountVoucher(UUID.randomUUID(), 10L));
+
+        OrderService orderService = applicationContext.getBean(OrderService.class);
+        Order order = orderService.createOrder(customerId, new ArrayList<>() {{
+            add(new OrderItem(UUID.randomUUID(), 100L, 1));
+        }}, voucher.getVoucherId());
+
+        Assert.isTrue(order.totalAmount() == 90L,
+            MessageFormat.format("totalAmount {0} is not 100L", order.totalAmount()));
+
     }
 }
