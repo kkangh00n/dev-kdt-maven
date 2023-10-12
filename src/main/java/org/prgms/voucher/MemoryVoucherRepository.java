83b6bcecb018abba2a3d759347beeb8210295cff
kkangh00n <kig2454@gmail.com>
2023-10-12 18:27:40 +0900

    feat: bean 객체 생성 & 소멸 Life Cycle 콜백

--- src/main/java/org/prgms/voucher/MemoryVoucherRepository.java
+++ src/main/java/org/prgms/voucher/MemoryVoucherRepository.java
@@ -1,9 +1,13 @@
 package org.prgms.voucher;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
@@ -12,9 +16,10 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 @Qualifier("memory")
-@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 //@Primary
-public class MemoryVoucherRepository implements VoucherRepository {
+public class MemoryVoucherRepository implements VoucherRepository, InitializingBean,
+    DisposableBean {
     private final Map<UUID, Voucher> storage = new ConcurrentHashMap<>();
 
     @Override
@@ -26,5 +31,26 @@ public class MemoryVoucherRepository implements VoucherRepository {
     public Voucher insert(Voucher voucher) {
         storage.put(voucher.getVoucherId(), voucher);
         return voucher;
+    }
+
+    @PostConstruct
+    public void postConstruct(){
+        System.out.println("postConstruct called");
+    }
+
+
+    @Override
+    public void afterPropertiesSet() {
+        System.out.println("afterPropertiesSet called");
+    }
+
+    @PreDestroy
+    public void preDestroy(){
+        System.out.println("preDestroy called");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy called");
     }
 }
