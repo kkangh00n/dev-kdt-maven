ff8901128e4ed90aa5d7d5fd4d0b5a925112aeb1
kkangh00n <kig2454@gmail.com>
2023-10-12 16:31:07 +0900

    refactor: 스테레오 타입 어노테이션(@Repository)을 이용한 bean 등록 & 기능 추가

---
+++ src/main/java/org/prgms/voucher/MemoryVoucherRepository.java
@@ -0,0 +1,23 @@
+package org.prgms.voucher;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemoryVoucherRepository implements VoucherRepository {
+    private final Map<UUID, Voucher> storage = new ConcurrentHashMap<>();
+
+    @Override
+    public Optional<Voucher> findById(UUID voucherId) {
+        return Optional.ofNullable(storage.get(voucherId));
+    }
+
+    @Override
+    public Voucher insert(Voucher voucher) {
+        storage.put(voucher.getVoucherId(), voucher);
+        return voucher;
+    }
+}
--- src/main/java/org/prgms/voucher/VoucherRepository.java
+++ src/main/java/org/prgms/voucher/VoucherRepository.java
@@ -1,9 +1,9 @@
-package org.prgms;
+package org.prgms.voucher;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface VoucherRepository {
-
     Optional<Voucher> findById(UUID voucherId);
+    Voucher insert (Voucher voucher);
 }
