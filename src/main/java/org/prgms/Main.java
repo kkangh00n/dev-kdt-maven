fecf5fb069305f02396563e5afb5a9546f23483d
kkangh00n <kig2454@gmail.com>
2023-10-13 23:37:39 +0900

    feat: 외부 리소스 갖고오기

--- src/main/java/org/prgms/Main.java
+++ src/main/java/org/prgms/Main.java
@@ -1,9 +1,19 @@
 package org.prgms;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.prgms.order.Order;
 import org.prgms.order.OrderItem;
 import org.prgms.order.OrderProperties;
@@ -15,12 +25,14 @@ import org.prgms.voucher.VoucherRepository;
 import org.springframework.beans.factory.annotation.BeanFactoryAnnotationUtils;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.io.Resource;
 import org.springframework.util.Assert;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+
         applicationContext.register(AppConfiguration.class);
         ConfigurableEnvironment environment = applicationContext.getEnvironment();
         environment.setActiveProfiles("dev");
@@ -35,11 +47,34 @@ public class Main {
 //        System.out.println(MessageFormat.format("supportVendors -> {0}", supportVendors));
 //        System.out.println(MessageFormat.format("description -> {0}", description));
 
-//        OrderProperties orderProperties = applicationContext.getBean(OrderProperties.class);
+        OrderProperties orderProperties = applicationContext.getBean(OrderProperties.class);
 //        System.out.println(MessageFormat.format("version -> {0}", orderProperties.getVersion()));
 //        System.out.println(MessageFormat.format("minimumOrderAmount -> {0}", orderProperties.getMinimumOrderAmount()));
 //        System.out.println(MessageFormat.format("supportVendors -> {0}", orderProperties.getSupportVendors()));
 //        System.out.println(MessageFormat.format("description -> {0}", orderProperties.getDescription()));
+
+
+        Resource resource = applicationContext.getResource("application.yaml");
+        Resource resource2 = applicationContext.getResource("file:sample.txt");
+
+        System.out.println("resource = " + resource2.getClass().getCanonicalName());
+        File file = resource2.getFile();
+        List<String> strings = Files.readAllLines(file.toPath());
+        System.out.println(strings.stream().reduce("", (a,b) -> a+"\n"+b));
+
+
+
+        Resource resource3 = applicationContext.getResource("https://stackoverflow.com/");
+
+        InputStream inputStream = resource3.getURL().openStream();
+        ReadableByteChannel readableByteChannel = Channels.newChannel(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(
+            Channels.newReader(readableByteChannel, StandardCharsets.UTF_8));
+        Stream<String> lines = bufferedReader.lines();
+        String content = lines.collect(Collectors.joining("\n"));
+        System.out.println(content);
+
+
 
         UUID customerId = UUID.randomUUID();
         VoucherRepository voucherRepository = applicationContext.getBean(VoucherRepository.class);
