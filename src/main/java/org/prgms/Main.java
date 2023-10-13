795bbc0ab4f33bf5f28064dd5f398b511b708c85
kkangh00n <kig2454@gmail.com>
2023-10-13 11:36:04 +0900

    feat: yaml파일 변수 출력

--- src/main/java/org/prgms/Main.java
+++ src/main/java/org/prgms/Main.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.UUID;
 import org.prgms.order.Order;
 import org.prgms.order.OrderItem;
+import org.prgms.order.OrderProperties;
 import org.prgms.order.OrderService;
 import org.prgms.voucher.FixedAmountVoucher;
 import org.prgms.voucher.Voucher;
@@ -21,14 +22,21 @@ public class Main {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
             AppConfiguration.class);
 
-        ConfigurableEnvironment environment = applicationContext.getEnvironment();
-        String version = environment.getProperty("kdt.version");
-        Integer minimumOrderAmount = environment.getProperty("kdt.minimum-order-amount", Integer.class);
-        List<String> supportVendors = environment.getProperty("kdt.support-vendors", List.class);
-        System.out.println(MessageFormat.format("version -> {0}", version));
-        System.out.println(MessageFormat.format("minimumOrderAmount -> {0}", minimumOrderAmount));
-        System.out.println(MessageFormat.format("supportVendors -> {0}", supportVendors));
+//        ConfigurableEnvironment environment = applicationContext.getEnvironment();
+//        String version = environment.getProperty("kdt.version");
+//        Integer minimumOrderAmount = environment.getProperty("kdt.minimum-order-amount", Integer.class);
+//        List<String> supportVendors = environment.getProperty("kdt.support-vendors", List.class);
+//        List<String> description = environment.getProperty("kdt.description", List.class);
+//        System.out.println(MessageFormat.format("version -> {0}", version));
+//        System.out.println(MessageFormat.format("minimumOrderAmount -> {0}", minimumOrderAmount));
+//        System.out.println(MessageFormat.format("supportVendors -> {0}", supportVendors));
+//        System.out.println(MessageFormat.format("description -> {0}", description));
 
+        OrderProperties orderProperties = applicationContext.getBean(OrderProperties.class);
+        System.out.println(MessageFormat.format("version -> {0}", orderProperties.getVersion()));
+        System.out.println(MessageFormat.format("minimumOrderAmount -> {0}", orderProperties.getMinimumOrderAmount()));
+        System.out.println(MessageFormat.format("supportVendors -> {0}", orderProperties.getSupportVendors()));
+        System.out.println(MessageFormat.format("description -> {0}", orderProperties.getDescription()));
 
         UUID customerId = UUID.randomUUID();
         VoucherRepository voucherRepository = BeanFactoryAnnotationUtils.qualifiedBeanOfType(
