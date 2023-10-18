fa87b3b1c12927c68356c927fb364d6a8bc996e7
kkangh00n <kig2454@gmail.com>
2023-10-18 19:19:48 +0900

    feat: JDBC 실습 2 (SQL injection)

--- src/main/java/org/prgms/JdbcCustomerRepository.java
+++ src/main/java/org/prgms/JdbcCustomerRepository.java
@@ -5,26 +5,43 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.MessageFormat;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class JdbcCustomerRepository {
     private static final Logger logger = LoggerFactory.getLogger(JdbcCustomerRepository.class);
-    public static void main(String[] args){
+
+    public List<String> findNames(String name){
+        String SELECT_SQL = "select * from customers WHERE name= '%s'". formatted(name);
+        List<String> names = new ArrayList<>();
+
         try(        //DB 커넥션은 많은 resource를 차지
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/order_mgmt", "root", "0000");
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from customers");
+            ResultSet resultSet = statement.executeQuery(SELECT_SQL);
         ) {
             while(resultSet.next()){
-                String name = resultSet.getString("name");
+                String customerName = resultSet.getString("name");
                 UUID customerId = UUID.nameUUIDFromBytes(resultSet.getBytes("customer_id"));
-                logger.info("customer id -> {}, name -> {}", customerId, name);
+                LocalDateTime createAt = resultSet.getTimestamp("create_at").toLocalDateTime();
+                logger.info("customer id -> {}, name -> {}, created_at -> {}", customerId, name, createAt);
+                names.add(customerName);
             }
         } catch (SQLException e) {
             e.printStackTrace();
             logger.error("connection closing 하는 동안 error", e);
         }
+
+        return names;
+    }
+
+    public static void main(String[] args){
+        List<String> names = new JdbcCustomerRepository().findNames("tester01' OR 'a'='a");         //SQL injection -> 누군가 악의적으로 sql을 조작 -> 보안에 취약
+        names.forEach(v -> logger.info("Found name : {}", v));
     }
 }
