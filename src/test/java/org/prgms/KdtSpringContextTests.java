b7f0002989a6484c167114467e8b9ecbfad1e8a0
kkangh00n <kig2454@gmail.com>
2023-10-18 15:22:15 +0900

    test: VoucherRepository를 Config 파일을 통해 Context에 등록하고 bean으로 갖고오기

--- src/test/java/org/prgms/KdtSpringContextTests.java
+++ src/test/java/org/prgms/KdtSpringContextTests.java
@@ -3,18 +3,40 @@ package org.prgms;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 
+import java.util.Optional;
+import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.prgms.voucher.Voucher;
+import org.prgms.voucher.VoucherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 //Spring TestContext를 사용하도록 설정
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration
+@SpringJUnitConfig
 public class KdtSpringContextTests {
+
+    @Configuration
+    static class Config{
+        @Bean
+        VoucherRepository voucherRepository(){
+            return new VoucherRepository() {
+                @Override
+                public Optional<Voucher> findById(UUID voucherId) {
+                    return Optional.empty();
+                }
+
+                @Override
+                public Voucher insert(Voucher voucher) {
+                    return null;
+                }
+            };
+
+        }
+    }
 
     @Autowired
     ApplicationContext context;
@@ -23,5 +45,12 @@ public class KdtSpringContextTests {
     @DisplayName("applicationContext가 생성")
     void testApplicationContext() {
         assertThat(context, notNullValue());
+    }
+
+    @Test
+    @DisplayName("VoucherRepository가 빈으로 등록되어 있어야 한다.")
+    void testVoucherRepositoryCreation() {
+        VoucherRepository bean = context.getBean(VoucherRepository.class);
+        assertThat(bean, notNullValue());
     }
 }
