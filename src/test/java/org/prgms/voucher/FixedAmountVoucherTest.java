783384f95f5124db161fad92f4344d1c330a2983
kkangh00n <kig2454@gmail.com>
2023-10-18 13:43:45 +0900

    test: @BeforeEach, @BeforeAll

--- src/test/java/org/prgms/voucher/FixedAmountVoucherTest.java
+++ src/test/java/org/prgms/voucher/FixedAmountVoucherTest.java
@@ -2,13 +2,28 @@ package org.prgms.voucher;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.sun.nio.sctp.IllegalReceiveException;
 import java.util.UUID;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class FixedAmountVoucherTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(FixedAmountVoucherTest.class);
+
+    @BeforeAll
+    static void setup(){
+        logger.info("@BeforeAll - run once");
+    }
+
+    @BeforeEach
+    void init(){
+        logger.info("@BeforeEach - run before each test method");
+    }
 
     @Test
     @DisplayName("기본적인 assertEqual 테스트")
@@ -24,7 +39,7 @@ class FixedAmountVoucherTest {
 
     @Test
     @DisplayName("할인 금액은 마이너스가 될 수 없다.")
-    @Disabled     //잠시 테스트 제외 가능
+    @Disabled     //테스트 제외 가능
     void testWithMinus() {
         assertThrows(IllegalArgumentException.class, () -> new FixedAmountVoucher(UUID.randomUUID(), -100));
     }
