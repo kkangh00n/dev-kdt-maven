dedc5200900eabdc16117fee40dca93851d5bd49
kkangh00n <kig2454@gmail.com>
2023-10-19 20:20:54 +0900

    feat: JDBC Template 3 (JdbcTemplate을 이용하여 insert, update, delete 수정)

--- src/main/java/org/prgms/customer/CustomerJdbcRepository.java
+++ src/main/java/org/prgms/customer/CustomerJdbcRepository.java
@@ -48,40 +48,57 @@ public class CustomerJdbcRepository implements CustomerRepository {
 
     @Override
     public Customer insert(Customer customer) {
-        try(
-            Connection connection = dataSource.getConnection();
-            PreparedStatement statement = connection.prepareStatement("INSERT INTO customers(customer_id, name, email, created_at) VALUES (UUID_TO_BIN(?), ?, ?, ?)");
-        ) {
-            statement.setBytes(1, customer.getCustomerId().toString().getBytes());
-            statement.setString(2, customer.getName());
-            statement.setString(3, customer.getEmail());
-            statement.setTimestamp(4, Timestamp.valueOf(customer.getCreatedAt()));
-            int executeUpdate = statement.executeUpdate();
-            if(executeUpdate!=1) throw new RuntimeException("Nothing was inserted");
-            return customer;
-        } catch (SQLException e) {
-            logger.error("connection closing 하는 동안 error", e);
-            throw new RuntimeException(e);
-        }
+        int update = jdbcTemplate.update(
+            "INSERT INTO customers(customer_id, name, email, created_at) VALUES (UUID_TO_BIN(?), ?, ?, ?)",
+            customer.getCustomerId().toString().getBytes(),
+            customer.getName(),
+            customer.getEmail(),
+            Timestamp.valueOf(customer.getCreatedAt()));
+        if(update!=1) throw new RuntimeException("Nothing was inserted");
+        return customer;
+//        try(
+//            Connection connection = dataSource.getConnection();
+//            PreparedStatement statement = connection.prepareStatement("INSERT INTO customers(customer_id, name, email, created_at) VALUES (UUID_TO_BIN(?), ?, ?, ?)");
+//        ) {
+//            statement.setBytes(1, customer.getCustomerId().toString().getBytes());
+//            statement.setString(2, customer.getName());
+//            statement.setString(3, customer.getEmail());
+//            statement.setTimestamp(4, Timestamp.valueOf(customer.getCreatedAt()));
+//            int executeUpdate = statement.executeUpdate();
+//            if(executeUpdate!=1) throw new RuntimeException("Nothing was inserted");
+//            return customer;
+//        } catch (SQLException e) {
+//            logger.error("connection closing 하는 동안 error", e);
+//            throw new RuntimeException(e);
+//        }
     }
 
     @Override
     public Customer update(Customer customer) {
-        try(
-            Connection connection = dataSource.getConnection();
-            PreparedStatement statement = connection.prepareStatement("UPDATE customers SET name = ?, email = ?, last_login_at = ? where customer_id = UUID_TO_BIN(?)");
-        ) {
-            statement.setString(1, customer.getName());
-            statement.setString(2, customer.getEmail());
-            statement.setTimestamp(3, customer.getLastLoginAt()!=null ? Timestamp.valueOf(customer.getLastLoginAt()):null);
-            statement.setBytes(4, customer.getCustomerId().toString().getBytes());
-            int executeUpdate = statement.executeUpdate();
-            if(executeUpdate!=1) throw new RuntimeException("Nothing was inserted");
-            return customer;
-        } catch (SQLException e) {
-            logger.error("connection closing 하는 동안 error", e);
-            throw new RuntimeException(e);
-        }
+        int update = jdbcTemplate.update(
+            "UPDATE customers SET name = ?, email = ?, last_login_at = ? where customer_id = UUID_TO_BIN(?)",
+            customer.getName(),
+            customer.getEmail(),
+            customer.getLastLoginAt()!=null?Timestamp.valueOf(customer.getLastLoginAt()):null,
+            customer.getCustomerId().toString().getBytes()
+        );
+        if(update!=1) throw new RuntimeException("Nothing was inserted");
+        return customer;
+//        try(
+//            Connection connection = dataSource.getConnection();
+//            PreparedStatement statement = connection.prepareStatement("UPDATE customers SET name = ?, email = ?, last_login_at = ? where customer_id = UUID_TO_BIN(?)");
+//        ) {
+//            statement.setString(1, customer.getName());
+//            statement.setString(2, customer.getEmail());
+//            statement.setTimestamp(3, customer.getLastLoginAt()!=null ? Timestamp.valueOf(customer.getLastLoginAt()):null);
+//            statement.setBytes(4, customer.getCustomerId().toString().getBytes());
+//            int executeUpdate = statement.executeUpdate();
+//            if(executeUpdate!=1) throw new RuntimeException("Nothing was inserted");
+//            return customer;
+//        } catch (SQLException e) {
+//            logger.error("connection closing 하는 동안 error", e);
+//            throw new RuntimeException(e);
+//        }
     }
 
     @Override
@@ -109,16 +126,6 @@ public class CustomerJdbcRepository implements CustomerRepository {
 //            throw new RuntimeException(e);
 //        }
 //        return allCustomers;
-    }
-
-    private static void mapToCustomer(ResultSet resultSet, List<Customer> allCustomers) throws SQLException {
-        String customerName = resultSet.getString("name");
-        String email = resultSet.getString("email");
-        UUID customerId = toUUID(resultSet.getBytes("customer_id"));
-        LocalDateTime lastLoginAt = resultSet.getTimestamp("last_login_at") != null ? resultSet.getTimestamp(
-                "last_login_at").toLocalDateTime() : null;
-        LocalDateTime createdAt = resultSet.getTimestamp("created_at").toLocalDateTime();
-        allCustomers.add(new Customer(customerId, customerName, email, lastLoginAt, createdAt));
     }
 
     @Override
@@ -153,59 +160,76 @@ public class CustomerJdbcRepository implements CustomerRepository {
 
     @Override
     public Optional<Customer> findByName(String name) {
-        List<Customer> allCustomers = new ArrayList<>();
-
-        try(
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/order_mgmt", "root", "0000");
-            PreparedStatement statement = connection.prepareStatement("select * from customers WHERE name = ?");          //prepareStatement -> SQL injection 방지
-        ) {
-            statement.setString(1, name);
-            try(ResultSet resultSet = statement.executeQuery()){
-                while(resultSet.next()){
-                    mapToCustomer(resultSet, allCustomers);
-                }
-            }
-        } catch (SQLException e) {
-            logger.error("Gor error while closing connection", e);
-            throw new RuntimeException(e);
+        try{
+            return Optional.ofNullable(jdbcTemplate.queryForObject("select * from customers WHERE name = ?",
+                customerRowMapper,
+                name));
+        } catch (EmptyResultDataAccessException e){
+            logger.error("Got empty result", e);
+            return Optional.empty();
         }
-
-        return allCustomers.stream().findFirst();
+//        List<Customer> allCustomers = new ArrayList<>();
+//
+//        try(
+//            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/order_mgmt", "root", "0000");
+//            PreparedStatement statement = connection.prepareStatement("select * from customers WHERE name = ?");          //prepareStatement -> SQL injection 방지
+//        ) {
+//            statement.setString(1, name);
+//            try(ResultSet resultSet = statement.executeQuery()){
+//                while(resultSet.next()){
+//                    mapToCustomer(resultSet, allCustomers);
+//                }
+//            }
+//        } catch (SQLException e) {
+//            logger.error("Gor error while closing connection", e);
+//            throw new RuntimeException(e);
+//        }
+//
+//        return allCustomers.stream().findFirst();
     }
 
     @Override
     public Optional<Customer> findByEmail(String email) {
-        List<Customer> allCustomers = new ArrayList<>();
-
-        try(
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/order_mgmt", "root", "0000");
-            PreparedStatement statement = connection.prepareStatement("select * from customers WHERE email = ?");          //prepareStatement -> SQL injection 방지
-        ) {
-            statement.setString(1, email);
-            try(ResultSet resultSet = statement.executeQuery()){
-                while(resultSet.next()){
-                    mapToCustomer(resultSet, allCustomers);
-                }
-            }
-        } catch (SQLException e) {
-            logger.error("Gor error while closing connection", e);
-            throw new RuntimeException(e);
+        try{
+            return Optional.ofNullable(jdbcTemplate.queryForObject("select * from customers WHERE email = ?",
+                customerRowMapper,
+                email));
+        } catch (EmptyResultDataAccessException e){
+            logger.error("Got empty result", e);
+            return Optional.empty();
         }
-
-        return allCustomers.stream().findFirst();
+//        List<Customer> allCustomers = new ArrayList<>();
+//
+//        try(
+//            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/order_mgmt", "root", "0000");
+//            PreparedStatement statement = connection.prepareStatement("select * from customers WHERE email = ?");          //prepareStatement -> SQL injection 방지
+//        ) {
+//            statement.setString(1, email);
+//            try(ResultSet resultSet = statement.executeQuery()){
+//                while(resultSet.next()){
+//                    mapToCustomer(resultSet, allCustomers);
+//                }
+//            }
+//        } catch (SQLException e) {
+//            logger.error("Gor error while closing connection", e);
+//            throw new RuntimeException(e);
+//        }
+//
+//        return allCustomers.stream().findFirst();
     }
 
     @Override
     public void deleteAll() {
-        try(
-            Connection connection = dataSource.getConnection();
-            PreparedStatement statement = connection.prepareStatement("DELETE from customers");
-        ) {
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            logger.error("Gor error while closing connection", e);
-            throw new RuntimeException(e);
-        }
+        jdbcTemplate.update("DELETE FROM customers");
+//        try(
+//            Connection connection = dataSource.getConnection();
+//            PreparedStatement statement = connection.prepareStatement("DELETE from customers");
+//        ) {
+//            statement.executeUpdate();
+//        } catch (SQLException e) {
+//            logger.error("Gor error while closing connection", e);
+//            throw new RuntimeException(e);
+//        }
     }
 
     static UUID toUUID(byte[] bytes){
