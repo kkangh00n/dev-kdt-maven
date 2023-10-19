ee30e65177d2a1afc5b74482864c28f04118e791
kkangh00n <kig2454@gmail.com>
2023-10-19 13:42:09 +0900

    feat: DataSource - HikariCP (의존성 추가 후 도메인 생성)

---
+++ src/main/java/org/prgms/customer/Customer.java
@@ -0,0 +1,53 @@
+package org.prgms.customer;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Customer {
+
+    private final UUID customerId;
+    private String name;
+    private final String email;
+    private LocalDateTime lastLoginAt;
+    private final LocalDateTime createdAt;
+
+    public Customer(UUID customerId, String name, String email, LocalDateTime createdAt) {
+        validatedName(name);
+        this.customerId = customerId;
+        this.email = email;
+        this.createdAt = createdAt;
+    }
+
+    private void validatedName(String name){
+        if(name.isBlank()) throw new RuntimeException("Name shoud not be blank");
+    }
+
+    public void changeName(String name){
+        validatedName(name);
+        this.name = name;
+    }
+
+    public void login(){
+        this.lastLoginAt = LocalDateTime.now();
+    }
+
+    public UUID getCustomerId() {
+        return customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDateTime getLastLoginAt() {
+        return lastLoginAt;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
